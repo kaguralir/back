fdd0d2fd25adf170efca85199616ca9416907390
Hedwig Reinbacher <reinbacher.hedwig.simplon@gmail.com>
2022-01-04 17:00:42 +0100

    sql likes and job offers, unique keys, and more

--- database.sql
+++ database.sql
@@ -4,15 +4,15 @@ CREATE TABLE user (
   user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
   demo TINYINT NOT NULL DEFAULT(0),
   role VARCHAR(100),
-  organizationId INTEGER,
-  projectId INTEGER,
+  organizationId INTEGER (255),
+  projectId INTEGER (255),
+  CONSTRAINT UC_projectId UNIQUE (projectId),
   name VARCHAR(100),
   email VARCHAR(100),
   password VARCHAR(100),
-  mobile INTEGER,
+  mobile INTEGER (255),
   createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updatedAt DDATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  ATE
+  updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 DROP TABLE IF EXISTS searchedJob;
 CREATE TABLE searchedJob(
@@ -40,14 +40,44 @@ DROP TABLE IF EXISTS images;
 CREATE TABLE images (
   image_id INTEGER AUTO_INCREMENT PRIMARY KEY,
   uploadedFiles_id INTEGER(100),
-CONSTRAINT FK_uploadedFiles_id FOREIGN KEY (uploadedFiles_id) REFERENCES uploads(uploads_id),
+  CONSTRAINT FK_uploadedFiles_id FOREIGN KEY (uploadedFiles_id) REFERENCES uploads(uploads_id),
   filename varchar(255)
 );
-
 DROP TABLE IF EXISTS skillsCandidate;
 CREATE TABLE skillsCandidate (
   skillsCandidate_id INTEGER AUTO_INCREMENT PRIMARY KEY,
   job_id INTEGER(100),
   CONSTRAINT FK_job_id FOREIGN KEY (job_id) REFERENCES searchedJob(searchedJob_id)
-
+);
+DROP TABLE IF EXISTS jobOffers;
+CREATE TABLE jobOffers(
+  jobOffer_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  recruiter_id INTEGER(100),
+  CONSTRAINT FK_recruiter_id FOREIGN KEY (recruiter_id) REFERENCES user(user_id),
+  available TINYINT(1),
+  organizationName VARCHAR(100),
+  jobProject_id INTEGER(100),
+  CONSTRAINT FK_jobProject_id FOREIGN KEY (jobProject_id) REFERENCES user(projectId),
+  jobOffer_role VARCHAR(100),
+  jobOffer_description VARCHAR(100),
+  createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+DROP TABLE IF EXISTS candidatThumbsActivity;
+CREATE TABLE candidatThumbsActivity(
+  thumb_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  candidat_id INTEGER(100),
+  CONSTRAINT FK_candidatThumb_id FOREIGN KEY (candidat_id) REFERENCES user(user_id),
+  jobThumbActivity_id INTEGER(100),
+  CONSTRAINT FK_jobThumbActivity_id FOREIGN KEY (jobThumbActivity_id) REFERENCES jobOffers(jobOffer_id),
+  thumbStatus TINYINT(1)
+);
+DROP TABLE IF EXISTS recruiterThumbsActivity;
+CREATE TABLE recruiterThumbsActivity(
+  thumb_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  recruiterThumb_id INTEGER(100),
+  CONSTRAINT FK_recruiterThumb_id FOREIGN KEY (recruiterThumb_id) REFERENCES jobOffers(recruiter_id),
+  candidat_id INTEGER(100),
+  CONSTRAINT FK_candidatLiked_id FOREIGN KEY (candidat_id) REFERENCES user(user_id),
+  thumbStatus TINYINT(1)
 );
