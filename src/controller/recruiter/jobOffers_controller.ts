e13bcfdcaeeb63374e9339db502f5c49c919a118
Hedwig Reinbacher <reinbacher.hedwig.simplon@gmail.com>
2022-01-05 11:05:20 +0100

    ok

---
+++ src/controller/recruiter/jobOffers_controller.ts
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import { jobOffers_repository } from "../../repository/recruiter/jobOffers_repository";
+import {JobOffers} from '../../entity/recruiter/jobOffers_entity';
+import bcrypt from 'bcrypt';
+
+
+
+export const JobOffersController = Router();
+
+JobOffersController.get('/allJobs', async (req, res) => {
+    try {
+        const post = await jobOffers_repository.getAllJobOffers();
+
+        return res.status(200).json({
+            success: true,
+            count: post.length,
+            data: post
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
+});
+
+
+
+JobOffersController.post('/addJob', async (req, res) => {
+    try {
+        const newJob = await new JobOffers(req.body);
+       
+
+        newJob.jobProject_id= req.user.projectId;
+
+        await jobOffers_repository.addJob(newJob,req.user.);
+        res.status(201).json({
+            success: true,
+            count: newJob.length,
+            data: newJob
+            })
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+
