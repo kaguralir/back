7f93fe46508a4b5c123847d3a369be30719776b9
kaguralir <arsonistera@gmail.com>
2022-01-07 14:38:42 +0100

    repo fini interest

--- src/repository/interest/interest_repository.ts
+++ src/repository/interest/interest_repository.ts
@@ -9,63 +9,71 @@ export class interest_repository {
      * @returns {Promise<Interest[]>} 
      */
 
-    static async getCandidatesWhoApplied() {
-        const [rows] = await connection.query<RowDataPacket[]>(`SELECT * FROM interest WHERE jobApplied_id=?`);
+    static async candidateInterest( job_id, candidate_id,candidateApplication: Interest,) {
+        try {
+            const [addedApplication] = await connection.query<ResultSetHeader>('INSERT INTO interest (jobApplied_id,candidateWhoApplied_id) VALUES (?,?)',
+                [job_id.jobApplied_id, candidate_id.candidateWhoApplied_id]);
+            candidateApplication.interest_id = addedApplication.insertId;
+        }
+        catch (err) {
+            console.log("add application repo err is", err)
+        }
+
+    }
+
+
+    static async candidateAnswer( job_id, recruiter_id,candidateApplication: Interest) {
+        try {
+            const [addedApplication] = await connection.query<ResultSetHeader>('UPDATE interest SET interest = ?  WHERE candidateWhoApplied_id=? AND recruiterJobOffer_id=? AND interest=NULL',
+                [job_id.jobApplied_id, recruiter_id.recruiterJobOffer_id, candidateApplication.interest]);
+            candidateApplication.interest_id = addedApplication.insertId;
+        }
+        catch (err) {
+            console.log("add application repo err is", err)
+        }
+
+    }
+
+
+
+    static async recruiterInterest(interest: Interest,job_id, candidate_id) {
+        try {
+            const [addedUser] = await connection.query<ResultSetHeader>(`INSERT INTO interest (jobApplied_id,candidateWhoApplied_id,recruiterJobOffer_id) VALUES (?,?,?)`, [job_id.jobApplied_id, candidate_id.candidateWhoApplied_id, interest.recruiterJobOffer_id]);
+            interest.interest_id = addedUser.insertId;
+        }
+        catch (err) {
+            console.log("recruiterInterest repo err is", err)
+        }
+
+    }
+
+    static async recruiterAnswer(answer: Interest,job_id) {
+        try {
+            const [addedUser] = await connection.query<ResultSetHeader>(`UPDATE interest SET interest =? WHERE jobApplied_id = ? AND candidateWhoApplied_id=?`, [answer.interest]);
+            answer.interest_id= addedUser.insertId;
+        }
+        catch (err) {
+            console.log("recruiterAnswer repo err is", err)
+        }
+
+    }
+
+    static async getCandidatesWithInterest() {
+        const [rows] = await connection.query<RowDataPacket[]>(`SELECT * FROM user 
+        INNER JOIN interest WHERE user_id = candidateWhoApplied_id AND jobApplied_id=? AND interest IS NULL  `);
 
         return rows.map(row => new Interest({
             interest_id: row['interest_id'], jobApplied_id: row['jobApplied_id'], candidateWhoApplied_id: row['candidateWhoApplied_id'], recruiterJobOffer_id: row['recruiterJobOffer_id'], interest: row['interest']
         }));
 
     }
-    /* interest_id:row['interest_id'],jobApplied_id: row['jobApplied_id'], candidateWhoApplied_id: row['candidateWhoApplied_id'], recruiterJobOffer_id: row['recruiterJobOffer_id'], interest:row['interest']
-        interest_id,jobApplied_id,candidateWhoApplied_id,recruiterJobOffer_id,interest */
 
-    static async candidateApplying( job_id, candidate_id,candidateApplication: Interest,) {
-        try {
-            const [addedApplication] = await connection.query<ResultSetHeader>('INSERT INTO interest (jobApplied_id,candidateWhoApplied_id,interest) VALUES (?,?,?)',
-                [job_id.jobApplied_id, candidate_id.candidateWhoApplied_id, candidateApplication.interest]);
-            candidateApplication.interest_id = addedApplication.insertId;
-        }
-        catch (err) {
-            console.log("add application repo err is", err)
-        }
+    static async getCandidatesWithoutInterest() {
+        const [rows] = await connection.query<RowDataPacket[]>(`SELECT * FROM user LEFT OUTER JOIN interest ON user_id=candidateWhoApplied_id WHERE candidateWhoApplied_id IS NULL AND role="candidat";`);
 
-    }
-
-
-    static async candidateAnswer( job_id, candidate_id,candidateApplication: Interest) {
-        try {
-            const [addedApplication] = await connection.query<ResultSetHeader>('UPDATE interest SET interest = 1 WHERE jobApplied_id = ? AND candidateWhoApplied_id=?',
-                [job_id.jobApplied_id, candidate_id.candidateWhoApplied_id, candidateApplication.interest]);
-            candidateApplication.interest_id = addedApplication.insertId;
-        }
-        catch (err) {
-            console.log("add application repo err is", err)
-        }
-
-    }
-
-
-
-    static async recruiterLikingCandidate(likingCandidate: Interest,job_id, candidate_id) {
-        try {
-            const [addedUser] = await connection.query<ResultSetHeader>(`INSERT INTO interest (jobApplied_id,candidateWhoApplied_id,interest) VALUES (?,?,?)`, [user.demo, user.role, user.organizationId]);
-            likingCandidate.interest_id = addedUser.insertId;
-        }
-        catch (err) {
-            console.log("adduser repo err is", err)
-        }
-
-    }
-
-    static async recruiterAnswer(likingApplier: Interest,job_id) {
-        try {
-            const [addedUser] = await connection.query<ResultSetHeader>(`UPDATE interest SET interest = 1 WHERE jobApplied_id = ? AND candidateWhoApplied_id=?`, [user.demo, user.role, user.organizationI]);
-            likingApplier.interest_id= addedUser.insertId;
-        }
-        catch (err) {
-            console.log("adduser repo err is", err)
-        }
+        return rows.map(row => new Interest({
+            interest_id: row['interest_id'], jobApplied_id: row['jobApplied_id'], candidateWhoApplied_id: row['candidateWhoApplied_id'], recruiterJobOffer_id: row['recruiterJobOffer_id'], interest: row['interest']
+        }));
 
     }
 }
